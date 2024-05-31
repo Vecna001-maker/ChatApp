f632353ee61882601abd1b7237c2f8df7b70bdf1
6string-guy <sanskarjaiswal010@gmail.com>
2024-06-01 01:48:23 +0530

    void

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import chats from './data/data.js'
-
+import { config as configDotenv } from 'dotenv';
 const app = express();
-app.listen(5000, console.log("Chal rha hai 5000 par"));
+configDotenv();
+ const PORT = process.env.PORT || 5000; 
+app.listen(PORT, console.log(`Chal rha hai ${PORT} par`));
 app.get('/', (req, res) => {
     res.send(chats)
 })
