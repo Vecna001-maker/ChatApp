57772d918b1ef6d69ecf2f8e249236ed6204803c
6string-guy <sanskarjaiswal010@gmail.com>
2024-06-01 01:22:13 +0530

    server and testing on dummy data

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
+import chats from './data/data.js'
 
 const app = express();
 app.listen(5000, console.log("Chal rha hai 5000 par"));
 app.get('/', (req, res) => {
-    res.send("Api is Running")
+    res.send(chats)
 })
